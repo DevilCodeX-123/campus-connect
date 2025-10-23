ccbab665780cbf822c3aa630c7406da21722e131
DevilCodeX <bossdevil817@gmail.com>
2025-10-23 22:06:39 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,19 @@
 //   },
 // }));
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc'; // use SWC plugin
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
+
 
 export default defineConfig({
   plugins: [react()],
