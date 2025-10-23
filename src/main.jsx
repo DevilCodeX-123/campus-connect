db6a52461c9b4dfa82681501b8ab001b922f7d49
DevilCodeX <bossdevil817@gmail.com>
2025-10-23 18:30:13 +0530

    Update and rename main.tsx to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,13 @@
 // createRoot(document.getElementById("root")!).render(<App />);
 // src/main.tsx
 <script type="module" src="/src/main.tsx"></script>
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
